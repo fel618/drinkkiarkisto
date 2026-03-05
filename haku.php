7119b77ea37ef77387cadfe7d3aeb47eebf3ee8b
Yan Lugovoy <jan.lugovoy@gmail.com>
2026-03-05 11:57:32 +0200

    feat: search page style

--- haku.php
+++ haku.php
@@ -1,5 +1,42 @@
 <?php
 include("yhteys.php");
+?>
+
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+<meta charset="UTF-8">
+<title>Drinkkihaku</title>
+
+<link rel="stylesheet" href="munTyyli.css">
+
+</head>
+
+<body>
+
+<?php include("naviGuest.php"); ?>
+
+<h2>Hae drinkkejä</h2>
+
+<form method="post">
+
+Haku:
+<input type="text" name="haku">
+
+<br><br>
+
+<input type="radio" name="tyyppi" value="nimi" checked> Nimi
+<input type="radio" name="tyyppi" value="aines"> Ainesosa
+
+<br><br>
+
+<input type="submit" name="laheta" value="Lähetä">
+
+</form>
+
+<hr>
+
+<?php
 
 if (isset($_POST['laheta'])) {
 
@@ -7,13 +44,16 @@ if (isset($_POST['laheta'])) {
     $tyyppi = $_POST['tyyppi'];
 
     if ($haku == "") {
+
         $sql = "SELECT * FROM Drinkki WHERE Hyvaksytty=1";
-    }
-    else if ($tyyppi == "nimi") {
-        $sql = "SELECT * FROM Drinkki 
+
+    } else if ($tyyppi == "nimi") {
+
+        $sql = "SELECT * FROM Drinkki
                 WHERE Nimi LIKE '%$haku%' AND Hyvaksytty=1";
-    }
-    else {
+
+    } else {
+
         $sql = "SELECT DISTINCT d.* FROM Drinkki d
                 JOIN Drinkki_Aines da ON d.DrinkkiID = da.DrinkkiID
                 JOIN Aines a ON da.AinesID = a.AinesID
@@ -24,30 +64,32 @@ if (isset($_POST['laheta'])) {
 
     while ($row = $tulos->fetch_assoc()) {
 
+        echo "<div class='drinkki'>";
+
         echo "<h3>".$row['Nimi']."</h3>";
-        echo "Juomalaji: ".$row['Juomalaji']."<br>";
+        echo "<p><b>Juomalaji:</b> ".$row['Juomalaji']."</p>";
 
         $id = $row['DrinkkiID'];
+
         $aines = $conn->query("
-            SELECT a.Nimi, da.Maara 
+            SELECT a.Nimi, da.Maara
             FROM Drinkki_Aines da
             JOIN Aines a ON da.AinesID = a.AinesID
             WHERE da.DrinkkiID=$id
         ");
 
-        echo "Ainesosat:<br>";
+        echo "<p><b>Ainekset:</b></p>";
+
         while ($r = $aines->fetch_assoc()) {
             echo $r['Nimi']." ".$r['Maara']."<br>";
         }
 
-        echo "Ohje: ".$row['Valmistusohje']."<br><hr>";
+        echo "<p><b>Ohje:</b> ".$row['Valmistusohje']."</p>";
+
+        echo "</div><hr>";
     }
 }
 ?>
 
-<form method="post">
-Haku: <input type="text" name="haku"><br>
-<input type="radio" name="tyyppi" value="nimi" checked> Nimi
-<input type="radio" name="tyyppi" value="aines"> Ainesosa<br>
-<input type="submit" name="laheta" value="Lähetä">
-</form>
+</body>
+</html>
